7e651003f25302e9b1e213af0ff842a46603b0e9
sergiovolquez <sergiovolquez12@gmail.com>
2026-01-09 21:48:35 -0800

    week1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,11 @@
 
+// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-    
+    // Get the current year dynamically and update the footer
     const currentYear = new Date().getFullYear();
     document.getElementById("copyrightYear").innerHTML = currentYear;
 
-    
+    // Get the last modified date of the document and update the footer
     const lastModifiedDate = document.lastModified;
     document.getElementById("lastModified").innerHTML = lastModifiedDate;
 });
